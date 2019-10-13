63ef05af287cce6c75e4cd388b1c4f6e02a50091
Prasanna Mestha <prasannamesta@gmail.com>
2019-10-13 17:37:18 +0530

    add user signup and login

--- back-end/src/config.js
+++ back-end/src/config.js
@@ -1,3 +1,4 @@
 module.exports = {
     PORT: 4000,
+    JWT_SECRET: 'SSSHHH____HH',
 }
--- back-end/src/resolvers/Mutation.js
+++ back-end/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../config')
+
 function addActor (root, args, context, info) {
     return context.prisma.createActor({
         name: args.name,
@@ -30,8 +34,28 @@ function addMovie (root, args, context, info) {
     })
 }
 
+async function createUser (root, args, context, info) {
+    const password = await bcrypt.hash(args.password, 10)
+    const user = await context.prisma.createUser({ ...args, password })
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET)
+    return { token, user }
+}
+
+async function login (root, args, context, info) {
+    const user = await context.prisma.user({ username: args.username })
+    if (!user) throw new Error('No such user')
+
+    const valid = await bcrypt.compare(args.password, user.password)
+    if (!valid) throw new Error('Incorrect password')
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET)
+    return { token, user }
+}
+
 module.exports = {
     addActor,
     addDirector,
     addMovie,
+    createUser,
+    login,
 }
