caa455090ca44a5e753d9212a068105af862c71d
Prasanna Mestha <prasannamesta@gmail.com>
2019-10-13 18:46:17 +0530

    setup apollo client for graphql

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,25 +2,30 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Switch, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import { ApolloProvider } from '@apollo/react-hooks'
 
 import Home from './pages/home'
 
 import { Navbar } from './components/nav'
 import { AppContainer } from './components/layoutElements'
 
+import client from './apollo'
+
 const history = createBrowserHistory();
 
 const App = () => (
-    <Router history={history}>
-        <Navbar>
-            Scoutbase Assignment
-        </Navbar>
-        <AppContainer>
-            <Switch>
-                <Route exact path="/" component={Home} />
-            </Switch>
-        </AppContainer>
-    </Router>
+    <ApolloProvider client={client}>
+        <Router history={history}>
+            <Navbar>
+                Scoutbase Assignment
+            </Navbar>
+            <AppContainer>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </AppContainer>
+        </Router>
+    </ApolloProvider>
 )
 
 render(<App />, document.querySelector('.root'))
